7f643d8b2247df666a57b6f30eae357975af7950
syssun <2506715686@qq.com>
2019-10-10 16:54:03 +0800

    引入bootstrap

--- src/main/java/com/sys/eblog/EblogApplication.java
+++ src/main/java/com/sys/eblog/EblogApplication.java
@@ -1,14 +1,9 @@
 package com.sys.eblog;
-
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class EblogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EblogApplication.class, args);
     }
-
 }
--- src/main/java/com/sys/eblog/test/TestC.java
+++ src/main/java/com/sys/eblog/test/TestC.java
@@ -1,17 +1,25 @@
 package com.sys.eblog.test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-@RestController
+@Controller
 public class TestC {
     @Autowired
     private TestDao testDao;
 
 
     @GetMapping("/test")
+    @ResponseBody
     public List getInfO(){
         return testDao.getInfo();
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
     }
 }
