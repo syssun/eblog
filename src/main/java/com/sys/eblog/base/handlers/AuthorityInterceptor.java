1ce56ef99d2266a3833684f248b03d4e419895fc
syssun <2506715686@qq.com>
2019-10-10 18:16:32 +0800

    1

---
+++ src/main/java/com/sys/eblog/base/handlers/AuthorityInterceptor.java
@@ -0,0 +1,55 @@
+package com.sys.eblog.base.handlers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashSet;
+import java.util.Set;
+@Slf4j
+public class AuthorityInterceptor implements HandlerInterceptor {
+
+    private static final Set<String> INTERCEPT_URI = new HashSet<>();//拦截的URI
+
+    static {
+        INTERCEPT_URI.add("/user/login.html");
+        INTERCEPT_URI.add("/user/login");
+    }
+
+    /**
+     * 在请求处理之前进行调用（Controller方法调用之前）
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object object) throws Exception {
+        String uri = request.getRequestURI();
+        if (INTERCEPT_URI.contains(uri)) {
+            return true;
+        }
+       // log.info("拦截" + uri);
+        HttpSession session = request.getSession();
+        System.out.println("preHandle");
+        return true;
+    }
+
+    /**
+     * 请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object object, ModelAndView mv) throws Exception {
+        System.out.println("postHandle");
+
+    }
+
+    /**
+     * 在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行
+     * （主要是用于进行资源清理工作）
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception ex) throws Exception {
+        System.out.println("afterCompletion");
+    }
+}
---
+++ src/main/java/com/sys/eblog/base/handlers/WebMvcConfigurer.java
@@ -0,0 +1,19 @@
+package com.sys.eblog.base.handlers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        /**
+         * 拦截器按照顺序执行,如果不同拦截器拦截存在相同的URL，前面的拦截器会执行，后面的拦截器将不执行
+         */
+        registry.addInterceptor(new AuthorityInterceptor())
+                .addPathPatterns("/**/**");
+        super.addInterceptors(registry);
+    }
+}
