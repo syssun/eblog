f777876fde8b3170fcc723c0e603c8f3abbd4e03
syssun <2506715686@qq.com>
2019-10-22 17:33:47 +0800

    useradmin

---
+++ src/main/java/com/sys/eblog/moudles/admin/UserAdmin.java
@@ -0,0 +1,18 @@
+package com.sys.eblog.moudles.admin;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@CrossOrigin("*")
+public class UserAdmin {
+    //登录
+    //注册
+    //退出登录
+
+
+
+
+}
